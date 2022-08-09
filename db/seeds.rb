dc55ed0f1aa1c294fce3bf31aab1f8c2fda47af3
Elizabeth Nguyen <saper209@yahoo.com>
2022-08-09 14:44:42 -0700

    Practice

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
 Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
 Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
@@ -17,5 +17,7 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "Shirt", value: 2, company_id: c1.id, dev_id: d2.id)
+Freebie.create(item_name: "Keychain", value: 1, company_id: c2.id, dev_id: d2.id)
 
 puts "Seeding done!"
